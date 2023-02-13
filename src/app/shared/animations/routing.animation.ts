93081adce96c722a89c3f3d927882ac7e20f4d0f
FadiKrdiyeh <66fadiiiiii66@gmail.com>
2023-02-13 20:57:17 +0200

    Modify routing animation and fix the header

--- src/app/shared/animations/routing.animation.ts
+++ src/app/shared/animations/routing.animation.ts
@@ -2,7 +2,7 @@ import { animate, style, transition, trigger } from "@angular/animations";
 
 export const RoutingAnimation = trigger("routing", [
   transition("*<=>*", [
-    style({ opacity: 0, transform: "translateX(-30px) rotate(2deg)" }),
-    animate("500ms", style({ opacity: 1, transform: "translateX(0px) rotateZ(0deg)" }))
+    style({ opacity: 0, transform: "translateX(-30px)" }),
+    animate("500ms", style({ opacity: 1, transform: "translateX(0px)" }))
   ])
 ]);
